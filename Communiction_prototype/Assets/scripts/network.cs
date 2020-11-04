0c5f457d78927c857df2a3b3f017c97b60ee0051
John Heaphey <61230540+towerdef-game@users.noreply.github.com>
2020-11-04 17:51:55 +0000

    networking for tomb diver
    and started on some randomly generated questions for the spot the mistake

---
+++ Communiction_prototype/Assets/scripts/network.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class network : NetworkManager
+{
+    public override void OnStartClient()
+    {
+
+    }
+    public override void OnStartHost()
+    {
+
+    }
+}
