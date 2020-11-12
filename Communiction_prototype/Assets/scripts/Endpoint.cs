226afdc0e271495b765b027ee90dba570010cbc6
John Heaphey <61230540+towerdef-game@users.noreply.github.com>
2020-11-12 10:41:00 +0000

    scripting added

---
+++ Communiction_prototype/Assets/scripts/Endpoint.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Endpoint : MonoBehaviour
+{
+    public int playergoal;
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            playergoal++;
+        }
+
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            playergoal--;
+        }
+
+    }
+
+     void Update()
+    {
+        if (playergoal >= 4)
+        {
+            endgame();
+        }
+    }
+    public void endgame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
--- Communiction_prototype/Assets/scripts/playermovement.cs
+++ Communiction_prototype/Assets/scripts/playermovement.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class playermovement : MonoBehaviour
+//
+
+public class playermovement : MonoBehaviour// NetworkBehaviour
 {
     public float walkingSpeed = 7.5f;
     public float runningSpeed = 11.5f;
@@ -12,25 +14,41 @@ public class playermovement : MonoBehaviour
     public Camera playerCamera;
     public float lookSpeed = 2.0f;
     public float lookXLimit = 45.0f;
+    public float RotateSpeed = 30f;
+  //  public Vector3 spawnpoint;
+
+    public Image Map;
 
     CharacterController characterController;
     Vector3 moveDirection = Vector3.zero;
-    float rotationX = 0;
+ //   float rotationX = 0;
 
-    public GameObject mapImage;
+    
 
     [HideInInspector]
     public bool canMove = true;
 
-    void Start()
+  void Start()
     {
+      //  if (!hasAuthority)
+         //   Destroy(this);
         characterController = GetComponent<CharacterController>();
-
-     
+        //  spawnpoint = transform.position;
+        //   characterController.enabled= true;
+        //    playerCamera.enabled = true;
+        Map = GameObject.FindGameObjectWithTag("map1").GetComponent<Image>(); 
+       
     }
+   
+  //  public override void OnStartAuthority()
+  //  {
+   //     base.OnStartAuthority();
+   // }
 
     void Update()
     {
+      //  if (!hasAuthority) return;
+       
         // We are grounded, so recalculate move direction based on axes
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
@@ -49,15 +67,7 @@ public class playermovement : MonoBehaviour
         {
             moveDirection.y = movementDirectionY;
         }
-
-       // if (Input.GetKeyDown("M"))
-       // {
-       //     mapImage.SetActive(true);
-       // }
-      //  if (Input.GetKeyUp("M"))
-      //  {
-     //       mapImage.SetActive(false);
-      //  }
+  
         if (!characterController.isGrounded)
         {
             moveDirection.y -= gravity * Time.deltaTime;
@@ -65,14 +75,25 @@ public class playermovement : MonoBehaviour
 
         // Move the controller
         characterController.Move(moveDirection * Time.deltaTime);
+        if (Input.GetKey(KeyCode.Q))
+            transform.Rotate(-Vector3.up * RotateSpeed * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.E))
+            transform.Rotate(Vector3.up * RotateSpeed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.C))
+            Map.enabled=true;
+        if (Input.GetKeyUp(KeyCode.C))
+            Map.enabled=false;
 
         // Player and Camera rotation
-        if (canMove)
-        {
-            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
-            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
-            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
-            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
-        }
-    }
+        //  if (canMove)
+        // {
+        //     rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+        //     rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+        //    playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+        //   transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+        // }
+
+}
+  
 }
